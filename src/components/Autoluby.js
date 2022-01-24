8b72174c45e693c5ee67cfb4e5d4f406750dc3ea
Kasoke <alvarogfn@hotmail.com>
2022-01-24 20:55:32 -0300

    Autoluby logo

--- src/components/Autoluby.js
+++ src/components/Autoluby.js
@@ -1,13 +1,14 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import autolubyIcon from "../assets/autoluby.svg";
 import styles from "./styles/Autoluby.module.css";
 
 const Autoluby = () => {
   return (
-    <div className={styles.icon}>
+    <Link to="/" className={styles.icon}>
       <img src={autolubyIcon} alt="autoLuby" />
       <h1 className={styles.h1}>AutoLuby</h1>
-    </div>
+    </Link>
   );
 };
 
