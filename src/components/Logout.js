f991da1c21277a6cd2a8ca5d76b376cdc2a0659a
Kasoke <alvarogfn@hotmail.com>
2022-01-25 17:08:34 -0300

    logout button

--- src/components/Logout.js
+++ src/components/Logout.js
@@ -1,7 +1,14 @@
 import React from "react";
 import styles from "./styles/Logout.module.css";
+import { UserContext } from "../UserContext";
+
 const Logout = () => {
-  return <button className={styles.button}>Sair</button>;
+  const { logout } = React.useContext(UserContext);
+  return (
+    <button className={styles.button} onClick={logout}>
+      Sair
+    </button>
+  );
 };
 
 export default Logout;
